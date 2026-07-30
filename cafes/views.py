eddbf5f35990aa02be570c475fef02f10eaf2849
Aysha-skhan <ayeshasakkhan@gmail.com>
2026-07-31 02:32:11 +0500

    Add cache-first search: serve cafés from DB, call Google only on miss

--- cafes/views.py
+++ cafes/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
+from dotenv import load_dotenv
+import os
+import requests
+load_dotenv()
+from .models import Cafe
+api_key = os.environ["GOOGLE_API_KEY"]
 # Create your views here.
+# def search(request):
+#     return HttpResponse("Hello, CafeHunt")
+def search(request):
+    search_lat = 31.5186      # hardcoded for now
+    search_lng = 74.34589
+    nearby = Cafe.objects.filter(
+    latitude__lte = search_lat + 0.0045,
+    latitude__gte = search_lat - 0.0045,
+    longitude__lte = search_lng + 0.0045,
+    longitude__gte = search_lng - 0.0045,
+)
+    if nearby.exists():
+        results = []
+        for cafe in nearby:
+            cafe_dict={"name":cafe.name,"address":cafe.address,"rating":cafe.rating}
+            results.append(cafe_dict)
+        return JsonResponse(results, safe=False)   # safe=False — it's a list, same as before
+    else:
+        # --- Step 1: Nearby Search to get a list of café ids ---
+        search_url = "https://places.googleapis.com/v1/places:searchNearby"
+        search_headers = {
+            "Content-Type": "application/json",
+            "X-Goog-Api-Key": api_key,
+            "X-Goog-FieldMask": "places.id,places.displayName,places.location,places.rating,places.formattedAddress",
+        }
+        body = {
+            "includedTypes": ["cafe"],
+            "maxResultCount": 4,
+            "locationRestriction": {
+                "circle": {
+                    "center": {"latitude": search_lat, "longitude": search_lng},
+                    "radius": 500.0,
+                }
+            },
+        }
+
+        search_response = requests.post(search_url, headers=search_headers, json=body)
+        places = search_response.json()["places"]
+        res={}
+        for place in places:
+            obj, created = Cafe.objects.get_or_create(
+            place_id=place["id"],
+            defaults={"name":place["displayName"]["text"], "latitude":place["location"]["latitude"],"longitude":place["location"]["longitude"],
+                    "rating":place.get("rating"), "address":place.get("formattedAddress")},
+        )
+            if created==True:
+                res[place["displayName"]["text"]]="Added to Cache"
+
+        return JsonResponse(res)
